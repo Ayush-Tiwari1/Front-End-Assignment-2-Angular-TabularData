c1cdc70385a767012c2ffe412dcee3a2e611c90e
Ayush Tiwari <ayushtiwari@Ayushs-MacBook-Pro.local>
2023-01-29 15:20:20 +0530

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularDemo';
+export class AppComponent implements OnInit {
+  public users: any=[];
+  constructor(private _appService:AppService){}
+  
+  ngOnInit(): void {
+    this._appService.getUsers().subscribe(users=>{
+      this.users=users;
+    })
+  }
 }
